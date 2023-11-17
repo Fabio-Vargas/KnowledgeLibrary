569a1a27e32f64a346df2fd0ab187180ec04690a
Fábio Vargas <fabio.gp.vargas@gmail.com>
2023-11-17 11:08:17 -0300

    Create 05-OperadoresLogicos.js

---
+++ JavaScript/05-OperadoresLogicos.js
@@ -0,0 +1,11 @@
+// ## Operadores Lógicos
+//    **( Operador ) Descrição**
+
+//      ( && ) AND (logic)
+if (condition1 && condition2) {code}
+
+//      ( || ) OR (logic)
+if (condition1 || condition2) {code}
+
+//      ( ! ) NOT (logic)
+if (!condition) {code}
