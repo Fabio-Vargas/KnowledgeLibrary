1d01f15cb2e5145f73faab9b8f1c73abd1c0cb53
Fábio Vargas <fabio.gp.vargas@gmail.com>
2023-11-17 11:01:01 -0300

    Create 02-OperadoresIncrementoDecremento.js

---
+++ JavaScript/02-OperadoresIncrementoDecremento.js
@@ -0,0 +1,16 @@
+// ## Operadores de Incremento e Decremento
+//    **( Operador ) Descrição**
+
+//      ( ++ ) Incremento | let contador = 0; contador++;
+let contadorIncremento = 1
+contador++
+contador++
+console.log("Resultado Contador Incremeto" + contadorIncremento)
+
+//      ( ++ ) Decremento | let contador = 0; contador--;
+let contadorDecremento = 1
+contador--
+contador--
+contador--
+contador--
+console.log("Resultado Contador Decremeto" + contadorDecremento)
