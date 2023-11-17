644f653caaa04e57eac25d34c264ffcf6f62081d
Fábio Vargas <fabio.gp.vargas@gmail.com>
2023-11-17 11:02:02 -0300

    Create 03-OperadoresAtribuicao.js

---
+++ JavaScript/03-OperadoresAtribuicao.js
@@ -0,0 +1,25 @@
+// ## Operadores de Atribuição
+//    **( Operador ) Descrição**
+
+//      ( = ) Atribuição | let x = num;
+let preco = 10;
+
+//      ( += ) Adição e Atribuição | let num = 10; num += 2;
+preco += 2 // preco = preco + 2
+console.log("Resultado Adição e Atribuição: " + preco)
+
+//      ( -= ) Subtração e Atribuição | let num = 10; num -= 3;
+preco -= 3 // preco = preco - 3
+console.log("Resultado Subtração e Atribuição: " + preco)
+
+//      ( *= ) Multiplicação e Atribuição | let num = 10; num *= 4;
+preco *= 4 // preco = preco * 4
+console.log("Resultado Multiplicação e Atribuição: " + preco)
+
+//      ( /= ) Divisão e Atribuição | let num = 10; num /= 5;
+preco /= 5 // preco = preco / 5
+console.log("Resultado Divisão e Atribuição: " + preco)
+
+//      ( %= ) Módulo e Atribuição | let num = 10; num %= 6;
+preco %= 6 // preco = preco % 6
+console.log("Resultado Módulo e Atribuição: " + preco)
