748df39867016b2ab19433095277e8c1504b3868
Fábio Vargas <fabio.gp.vargas@gmail.com>
2023-11-17 11:25:42 -0300

    Update 04-OperadoresComparacao(Relacionais).js

--- JavaScript/04-OperadoresComparacao(Relacionais).js
+++ JavaScript/04-OperadoresComparacao(Relacionais).js
@@ -1,21 +1,36 @@
 // ## Operadores de Comparação (Relacionais)
 //    **( Operador ) Descrição**
 
-//      ( == ) Equal to
-let isEqual = (x == y) // Value
-let isEqual2 = (x === y) // Value and Format
+//      ( == ) Equal To | Value: let isEqual = (x == y) | Value and Format: let isEqual = (x === y)
+let marca1 = "Apple"
+let resultado = marca === "Samsung"
+console.log(resultado)
 
-//      ( != ) Not Equal to
-let isNotEqual = (x != y)
+//      ( != ) Not Equal To | let isNotEqual = (x != y)
+let marca = "Apple"
+console.log(marca !== "Samsung")
 
-//      ( > ) Greater Than
-let isGreater = (x > y)
+//      ( > ) Greater Than | let isGreater = (x > y)
+let CPF_Bloqueado1 = "111.222.333-00"
+let CPF_Usuario1 = "222.333.444-00"
+let CPF_Autorizado1 = CPF_Usuario1 !== CPF_Bloqueado1
+console.log("Usuario autorizado? " + CPF_Autorizado1)
 
-//      ( < ) Less Than
-let isLess = (x < y)
+//      ( < ) Less Than | let isLess = (x < y)
+let CPF_Permitido2 = "111.222.333-00"
+let CPF_Usuario2 = "111.222.333-00"
+let CPF_Autorizado2 = CPF_Usuario2 !== CPF_Permitido2
+console.log("Usuario autorizado? " + CPF_Autorizado2)
 
-//      ( >= ) Greater Than or Equal to
-let isGreaterOrEqual = (x >= y)
+//      ( >= ) Greater Than or Equal To | let isGreaterOrEqual = (x >= y)
+let idadeMinima = 18
+let idadeUsuario = 18
+let idadePermitida = idadeUsuario >= idadeMinima
+console.log("Usuario maior de idade? ") + idadePermitida)
 
-//      ( <= ) Less Than or Equal to
-let isLessOrEqual = (x <= y)
+//      ( <= ) Less Than or Equal To | let isLessOrEqual = (x <= y)
+let idadeDeCorte = 60
+let idadeUsuario = 50
+let resultadoTerceiraIdade = idadeDeCorte <= idadeUsuario
+console.log("Usuario idoso? " + resultadoTerceiraIdade)
+
