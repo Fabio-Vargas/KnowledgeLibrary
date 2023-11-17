4e3cfef47de1bc9da15d1cee604ff1d9a052c0b0
Fábio Vargas <fabio.gp.vargas@gmail.com>
2023-11-17 11:57:23 -0300

    Create 07-EstruturaSwitchCase.js

---
+++ JavaScript/07-EstruturaSwitchCase.js
@@ -0,0 +1,23 @@
+
+
+let dia = "Terça"
+
+switch (dia)
+{
+  case "Segunda":
+    console.log("Hoje é Segunda-feira!")
+    break
+  case "Terça":
+    console.log("Hoje é Terça-feira!")
+    break
+  case "Quarta":
+    console.log("Hoje é Quarta-feira!")
+    break
+  case "Quinta":
+    console.log("Hoje é Quinta-feira!")
+    break
+  case "Sexta":
+    console.log("Hoje é Sexta-feira!")
+    break
+  default:
+    console.log("Final de Semana!")
