f55242d36ce89740abe65085facbd8efa5532d09
Fábio Vargas <fabio.gp.vargas@gmail.com>
2023-11-17 11:00:17 -0300

    Create OperadoresAritmeticos.js

---
+++ JavaScript/OperadoresAritmeticos.js
@@ -0,0 +1,35 @@
+// ## Operadores Aritméticos
+//    **( Operador ) Descrição**
+
+//      ( + ) Adição de Números | let adicaoNumeros = num1 + num2;
+let adicaoNum1 = 5
+let adicaoNum2 = 3
+let adicaoCalculo = adicaoNum1 + adicaoNum2
+console.log("Resultado do cálculo da Adição: " + adicaoCalculo)
+
+//      ( + ) Adição de Strings | let adicaoString = "string1" + "string2";
+let string1 = "A"
+let string2 = "B"
+console.log("Valor da variável: " + string1 + string2)
+
+//      ( - ) Subtração | let subtracao = num1 - num2
+let subtracaoNum1 = 5
+let subtracaoNum2 = 3
+let subtracaoCalculo = subtracaoNum1 - subtracaoNum2
+console.log("Resultado do cálculo da Subtração: " + subtracaoCalculo)
+
+//      ( * ) Multiplicação | let multiplicacao = num1 * num2
+let multiplicador = 5
+let multiplicando = 3
+let produto = multiplicador * multiplicando
+console.log("Resultado do cálculo da Multiplição: " + produto)
+
+//      ( / ) Divisão | let divisao = 5 / 3
+let divisor = 50
+let dividendo = 2
+let divisao = divisor / dividendo
+console.log("Resultado do cálculo da Divisão: " + divisao)
+
+//      ( % ) Módulo (Resto da Divisão) | let modulo = 5 % 3
+let modulo = 5 % 3
+console.log("O resto da Divisão é: " + modulo)
