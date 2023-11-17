d6d105e2f0e1c9a7ccd868afe8fa24ff1d570fc7
Fábio Vargas <fabio.gp.vargas@gmail.com>
2023-11-17 16:15:45 -0300

    Update 01-DataStructure.js

--- JSON/01-DataStructure.js
+++ JSON/01-DataStructure.js
@@ -1,12 +1,40 @@
 //JSON - JavaScript Object Notation
+
+// Single Object
+let objectSingle = {
+	name: "Fulano", 
+	age: 20, 
+	products: {
+		0: ["Item1", 10.90],
+		1: ["Item2", 100.90],
+		2: ["Item3", 1000.90],
+		3: ["Item4", 10000.90]
+	},
+	taxes: 100
+}
+
+// Multiple Objects
+let objectMultiple = [
+	{ "name": "Item1", "color": "Cor1", "type": "Tipo1" },
+	{ "name": "Item2", "color": "Cor2", "type": "Tipo2" }
+]
+
+let objectChosen = objectMultiple[0]
+console.log("O " + objectChosen.name + " é " + objectChosen.color + "!")
+
+let JSON_fileSharing = JSON.stringify(object)
+console.log(JSON_fileSharing)
+
+
+// EXAMPLE
 let invoice = {
 	name: "Fulano",
     age: 20,
     products: {
     	0: ["Item1", 10.90],
-      1: ["Item2", 100.90],
-      2: ["Item3", "1000.90"],
-      3: ["Item4", "10000.90"]
+		1: ["Item2", 100.90],
+		2: ["Item3", "1000.90"],
+		3: ["Item4", "10000.90"]
     },
     taxes: "98.90"
 }
