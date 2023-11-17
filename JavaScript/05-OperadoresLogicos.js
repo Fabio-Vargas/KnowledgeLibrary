d2447f585c9dc25509f7b94b283660f7c95b0448
Fábio Vargas <fabio.gp.vargas@gmail.com>
2023-11-17 11:34:21 -0300

    Update 05-OperadoresLogicos.js

--- JavaScript/05-OperadoresLogicos.js
+++ JavaScript/05-OperadoresLogicos.js
@@ -1,11 +1,29 @@
 // ## Operadores Lógicos
-//    **( Operador ) Descrição**
+//    **( Operador ) Descrição | Exemplo**
 
-//      ( && ) AND (logic)
-if (condition1 && condition2) {code}
+//      ( && ) AND (logic) | if (condition1 && condition2)
+let idade = 18
+let vistoVerificado = false
+let resultado1 = (idade >= 18) && (vistoVerificado === true)
+console.log(resultado1)
 
-//      ( || ) OR (logic)
-if (condition1 || condition2) {code}
+let moedasColetadas = 99
+let item1 = "Estrela"
+let resultado2 = (moedasColetadas >= 100) && (item1 === "Estrela")
+console.log(resultado2)
 
-//      ( ! ) NOT (logic)
-if (!condition) {code}
+//      ( || ) OR (logic) | if (condition1 || condition2)
+let tempo1 = "Chuva"
+let item2 = "Guarda Chuva"
+let podeSair = (tempo1 !== "Chuva") || (item2 === "Guarda Chuva")
+console.log("O personagem pode sair de casa? " + podeSair)
+
+//      ( ! ) NOT (logic) | if (!condition)
+let tempo2 = "Chuva"
+let resultado3 = tempo2 === "Chuva"
+console.log(!!resultado3)
+
+let tempo3 = "Chuva"
+let horario = 0800
+let resultado4 = !((tempo3 !== "Chuva") && (horario > 0600))
+console.log(reusltado4)
