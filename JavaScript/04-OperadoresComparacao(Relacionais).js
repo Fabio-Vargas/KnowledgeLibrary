ae031616dfbedbd45b0be851fe9c3f27c3ed2a13
Fábio Vargas <fabio.gp.vargas@gmail.com>
2023-11-17 11:07:47 -0300

    Create 04-OperadoresComparacao(Relacionais).js

---
+++ JavaScript/04-OperadoresComparacao(Relacionais).js
@@ -0,0 +1,21 @@
+// ## Operadores de Comparação (Relacionais)
+//    **( Operador ) Descrição**
+
+//      ( == ) Equal to
+let isEqual = (x == y) // Value
+let isEqual2 = (x === y) // Value and Format
+
+//      ( != ) Not Equal to
+let isNotEqual = (x != y)
+
+//      ( > ) Greater Than
+let isGreater = (x > y)
+
+//      ( < ) Less Than
+let isLess = (x < y)
+
+//      ( >= ) Greater Than or Equal to
+let isGreaterOrEqual = (x >= y)
+
+//      ( <= ) Less Than or Equal to
+let isLessOrEqual = (x <= y)
