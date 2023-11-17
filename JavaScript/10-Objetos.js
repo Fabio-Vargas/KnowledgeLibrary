b0dcba2603e5c32bdac0a23e158b808844561101
Fábio Vargas <fabio.gp.vargas@gmail.com>
2023-11-17 15:57:25 -0300

    Update 10-Objetos.js

--- JavaScript/10-Objetos.js
+++ JavaScript/10-Objetos.js
@@ -1,1 +1,21 @@
+// Criando um Objeto (Lembrar de colocar " , " após cada propriedade criada.
+let objeto = {
+  propriedade1: "valor",
+  propriedade2: valor,
+  propriedade3: "valor"
+}
 
+// Utilizando o Objeto criado
+console.log(objeto.propriedade1 + objeto.propriedade2 + objeto.propriedade3)
+  
+
+// EXEMPLOS
+let convidado = {
+  nome: "Fulano",
+  idade: 20,
+  presente: "Quebra-Cabeça",
+  localizacao: "Sala de Estar"
+}
+
+console.log(convidado.nome + " tem " + convidado.idade + " anos e trouxe um " + convidado.presente + " de presente!")
+console.log(convidado.nome + " está na " + convidado.localizacao + ".")
