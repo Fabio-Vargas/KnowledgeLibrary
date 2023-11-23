f77060e976e565ceaee678555f662afa189508ae
Fábio Vargas <fabio.gp.vargas@gmail.com>
2023-11-23 15:41:18 -0300

    Update 11-Classes.js

--- JavaScript/11-Classes.js
+++ JavaScript/11-Classes.js
@@ -1,8 +1,20 @@
-class ClassType {
-    attribute1
-    attribute2
+class Class {
+    attribute01
+    attribute02
 
-    function_action() {
+    functionAction() {
         console.log("Text ${this.attribute1} text ${this.attribute2}")
     }
 }
+
+const classInstance01 = new Class()
+classInstance01.attribute01 = "Text01"
+classInstance01.attribute02 = 01
+
+const classInstance02 = new Class()
+classInstance02.attribute01 = "Text02"
+classInstance02.attribute02 = 02
+
+classInstance01.functionAction()
+classInstance02.functionAction()
+
