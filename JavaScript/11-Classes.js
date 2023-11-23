f7b9d243c126c33722d17d407cec2c4dd02447ab
Fábio Vargas <fabio.gp.vargas@gmail.com>
2023-11-23 15:49:29 -0300

    Update 11-Classes.js

--- JavaScript/11-Classes.js
+++ JavaScript/11-Classes.js
@@ -1,20 +1,36 @@
+// Classe
 class Class {
     attribute01
     attribute02
 
+    // Todas as novas instancias da Classe necessitarão dos seguintes parâmetros para serem criadas.
+    constructor(attribute01, attribute02) {
+        this.attribute01 = attribute01
+        this.attribute02 = attribute02
+    }
+
+    
     functionAction() {
-        console.log("Text ${this.attribute1} text ${this.attribute2}")
+        console.log("Text ${this.attribute01} text ${this.attribute02}")
     }
 }
 
-const classInstance01 = new Class()
-classInstance01.attribute01 = "Text01"
-classInstance01.attribute02 = 01
+// Instancias da Classe
+    // Metodo 01
+    const classInstance01 = new Class()
+    classInstance01.attribute01 = "Text01"
+    classInstance01.attribute02 = 01
 
-const classInstance02 = new Class()
-classInstance02.attribute01 = "Text02"
-classInstance02.attribute02 = 02
+    const classInstance02 = new Class()
+    classInstance02.attribute01 = "Text02"
+    classInstance02.attribute02 = 02
 
-classInstance01.functionAction()
-classInstance02.functionAction()
+    classInstance01.functionAction()
+    classInstance02.functionAction()
+
+    // Metodo 02
+    const classInstance03 = new Class("Text03", 03)
+
+    classInstance03.functionAction()
+
 
