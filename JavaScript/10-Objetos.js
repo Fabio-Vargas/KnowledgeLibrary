c26bbed214f15fa0f0bf9ecb7743eaa8f43a6142
Fábio Vargas <fabio.gp.vargas@gmail.com>
2023-11-23 09:40:55 -0300

    Update 10-Objetos.js

--- JavaScript/10-Objetos.js
+++ JavaScript/10-Objetos.js
@@ -1,20 +1,25 @@
 // Criando um Objeto (Lembrar de colocar " , " após cada propriedade criada.
-let objeto = {
-  propriedade1: "valor",
-  propriedade2: valor,
-  propriedade3: "valor"
+let object = {
+    propriedade1: "valor",
+    propriedade2: valor,
+  
+    propriedade3: function action () {
+        console.log("Text ${this.propriedade1} text ${this.propriedade2}")
+    }
 }
 
 // Utilizando o Objeto criado
-console.log(objeto.propriedade1 + objeto.propriedade2 + objeto.propriedade3)
+console.log(object.propriedade1 + object.propriedade2)
+
+object.propriedade3()
   
 
 // EXEMPLOS
 let convidado = {
-  nome: "Fulano",
-  idade: 20,
-  presente: "Quebra-Cabeça",
-  localizacao: "Sala de Estar"
+    nome: "Fulano",
+    idade: 20,
+    presente: "Quebra-Cabeça",
+    localizacao: "Sala de Estar"
 }
 
 console.log(convidado.nome + " tem " + convidado.idade + " anos e trouxe um " + convidado.presente + " de presente!")
